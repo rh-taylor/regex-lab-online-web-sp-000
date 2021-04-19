6da8b1887f405fbf10dfcb33c1c61fb89496eb03
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-19 16:39:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,18 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else word.match(/\b^[^aeiouAEIOU]/)
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text_array = []
+  text_array << text.scan(/\b[uU]n[a-z]*ing\b/)
+  return text_array
 end
+
+
 
 def words_five_letters_long(text)
 
